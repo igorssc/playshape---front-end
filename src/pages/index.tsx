136cc6b25bde6e8ea3e1e64d9b74dc3b51735389
igorssc <igorsantoscosta@gmail.com>
2021-02-23 00:48:37 -0300

    Detecting dark system theme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,22 @@ import * as React from 'react'
 import { NextPage } from 'next'
 
 const IndexPage: NextPage = () => {
-  return <></>
+  const [isDark, setIsDark] = React.useState(false)
+
+  React.useEffect(() => {
+    const preferDarkQuery = '(prefers-color-scheme: dark)'
+    const mql = window.matchMedia(preferDarkQuery)
+    const supportsColorSchemeQuery = mql.media === preferDarkQuery
+
+    setIsDark(supportsColorSchemeQuery)
+  })
+  return (
+    <>
+      {isDark
+        ? 'Seu sistema está em modo escuro'
+        : 'Seu sistema não está em modo escuro'}
+    </>
+  )
 }
 
 export default IndexPage
