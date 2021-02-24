5e0ca88ceb0baeaa7398fe96d0489bed903abd33
igorssc <igorsantoscosta@gmail.com>
2021-02-24 18:56:01 -0300

    Correcting carousel padding

--- src/components/CarrouselIndex.tsx
+++ src/components/CarrouselIndex.tsx
@@ -44,21 +44,21 @@ const CarrouselIndex: React.FC = () => {
         breakpoint: 1024,
         settings: {
           slidesToShow: 4,
-          slidesToScroll: 2
+          slidesToScroll: 3
         }
       },
       {
         breakpoint: 768,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3
+          slidesToShow: 4,
+          slidesToScroll: 4
         }
       },
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToShow: 3,
+          slidesToScroll: 3
         }
       },
       {
